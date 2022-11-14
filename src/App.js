17462893d088151e07ff0f1454ed0b34c059a47a
marcosfeijoo <marcosfeijoo97@gmail.com>
2022-11-13 23:54:10 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import NavbarPrinc from './NavbarPrinc';
+
+function App() {
+  return (<div className="App">
+    
+ <Navbar/>
+ <NavbarPrinc/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavbarPrinc/index.js
@@ -0,0 +1,23 @@
+import './NavbarPrinc.css'
+import logo from '../assets/logo.png'
+const NavbarPrinc=()=>{
+    return (
+        <div>    <div className='Enrutado'><ul className='ulenrutado'>
+       
+           <img src={logo}></img>
+           <li>Inicio</li>
+            <li>Nosotros</li>
+            <li>Servicios</li>
+            <li>Productos</li>
+
+
+
+                </ul>
+                
+                </div>
+           </div>
+
+    
+    )
+}
+export default NavbarPrinc
