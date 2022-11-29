47ea94d87a9fa3812a190d22f0819867fb5b9017
Agustin Feijoo <agus99_feijoo@hotmail.com>
2022-11-29 17:09:44 -0300

    adding select

--- src/Componentes/ItemList/ItemList.js
+++ src/Componentes/ItemList/ItemList.js
@@ -2,9 +2,16 @@ import React, { useEffect,useState} from "react";
 import {getFirestore,collection,getDocs, DocumentSnapshot} from 'firebase/firestore';
 import './ItemList.css'
 import Select from "./Select";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const ItemList=()=>{
     const [products,setProducts]=useState([])
+    const metalsOptions = [
+        { label: "120x60x75", value: "500" },
+        { label: "160x80x75", value: "1000" },
+        { label: "2x100x75", value: "2000" },
+
+      ]
     useEffect(()=>{
         const db=getFirestore();
         const itemsCollection=collection(db,'Mesas');
@@ -26,7 +33,34 @@ const ItemList=()=>{
     <div>
     
 
-            </div><Select/></div>
+    </div>
+    <Formik
+        /*onSubmit={(values)=>updateCarrito({...values,props,"opcionesAdicionales":opcionesAdicionales},totalCalculado)}*/
+        initialValues={{
+            caños:"120x60x75"
+        }}
+        
+        >
+        {({
+        setFieldValue,
+        values,
+        },
+          ) => (
+            <Form>
+
+            <Field className="caños" id="caños" as="select" initialValue="120x60x75" name="caños">
+                {metalsOptions.map(pintura=>{
+                    return(<option key={pintura.label}  value={pintura.label}>{pintura.label}</option>)
+                })
+            }
+                                
+            </Field>
+            HOlaaaa {values.caños}
+                </Form>
+          )}
+          </Formik>
+     
+        </div>
           
   
     
